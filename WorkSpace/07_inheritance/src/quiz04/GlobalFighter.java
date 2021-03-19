f534c967d932c1c495d22f1cd0d757dc4e247694
ITSC <dhstjrxo123@gmail.com>
2021-03-19 09:48:53 +0900

    2021-03-19

--- WorkSpace/07_inheritance/src/quiz04/GlobalFighter.java
+++ WorkSpace/07_inheritance/src/quiz04/GlobalFighter.java
@@ -4,17 +4,26 @@ import java.util.Random;
 
 public class GlobalFighter extends Fighter {
 
-	public GlobalFighter(String name, int energy, int power) {
-		super(name, energy, power);
-		// TODO Auto-generated constructor stub
+	// field
+	
+	// constructor
+	public GlobalFighter(String name) {
+		super(name, (int)(Math.random() * 100) + 1, (int)(Math.random() * 10) + 1);
 	}
-
-	//attack() : 10% 확률로 한방에 ko시킬수 있다
+	
+	// attack() : 10% 확률로 한 방에 KO 시킬 수 있습니다.
 	@Override
 	public void attack(Fighter fighter) {
-		Random r = new Random();
-		
-		
+		// 나 : this
+		// 상대편 : fighter
+		if (Math.random() < 0.1) {  // 10% 확률
+			fighter.setEnergy(0);
+			System.out.println(fighter.getName() + " KO!");
+		} else {
+			// 내 power만큼 상대편 energy가 줄어듭니다.
+			fighter.setEnergy(fighter.getEnergy() - this.getPower());
+			System.out.println("공격: " + this.getName() + "[" + fighter.getName() + " 남은 에너지: " + fighter.getEnergy() + "]");
+		}
 	}
-
+	
 }
